029384fe4d731048b0151a893fc374e0067683ea
Furkan <furkanemmezoglu@outlook.com>
2020-03-23 20:28:09 +0300

    ben senin yerinde olsam bunuda cekmem
    cunku pomu felan degistirdim

--- src/main/java/com/digiadvert/ReklamOtomasyon/Service/SirketService.java
+++ src/main/java/com/digiadvert/ReklamOtomasyon/Service/SirketService.java
@@ -1,16 +1,21 @@
 package com.digiadvert.ReklamOtomasyon.Service;
 
+import com.digiadvert.ReklamOtomasyon.DataAccessLayer.HibernateSirketDal;
 import com.digiadvert.ReklamOtomasyon.DataAccessLayer.ISirketDal;
 import com.digiadvert.ReklamOtomasyon.HibernateEntities.Sirket;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import java.util.List;
 
 @Service
+@CrossOrigin(origins = "http://localhost:4200")
 public class SirketService implements ISirketService{
     private ISirketDal sirketDal;
+    private HibernateSirketDal hibernateSirketDal = new HibernateSirketDal();
+
     @Autowired
     public SirketService(ISirketDal sirketDal) {
         this.sirketDal = sirketDal;
@@ -19,12 +24,14 @@ public class SirketService implements ISirketService{
     @Override
     @Transactional
     public List<Sirket> getAll() {
+        System.out.println("SERVICE GET FUNCTION ");
         return this.sirketDal.getAll();
     }
 
     @Override
     @Transactional
     public void add(Sirket sirket) {
+        System.out.println("SERVICE GET FUNCTION ");
         this.sirketDal.add(sirket);
 
     }
